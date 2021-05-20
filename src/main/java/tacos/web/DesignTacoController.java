8201501232be8ad40a14b71126168777a7eefe7b
pkornijasz <pk@nkk.pl>
2021-05-20 16:16:58 +0200

    Initial commit

---
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,67 @@
+package tacos.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.Ingredient;
+import tacos.Taco;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import static tacos.Ingredient.*;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "pszenna", Type.WRAP),
+                new Ingredient("COTO", "kukurydziana", Type.WRAP),
+                new Ingredient("GRBF", "mielona wołowina ", Type.PROTEIN),
+                new Ingredient("CARN", "kawałki mięsa", Type.PROTEIN),
+                new Ingredient("TMTO", "pomidory pokrojone w kostkę", Type.VEGGIES),
+                new Ingredient("LETC", "sałata", Type.VEGGIES),
+                new Ingredient("CHED", "cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "pikantny sos pomidorowy", Type.SAUCE),
+                new Ingredient("SRCR", "śmietana", Type.SAUCE)
+        );
+
+        Type[] types = Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+    }
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    @PostMapping
+    public String processDesign() {
+        log.info("Przetwarzanie projektu taco: ");
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(
+            List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+}
